c520d41569b439e66d2f9523bbaec6d5ebdd7629
Sylvia L <sylvia@Sylvias-MBP.home>
2018-11-14 19:17:21 -1000

    presidents ex number 3

--- exercises.js
+++ exercises.js
@@ -45,7 +45,10 @@ This function will iterate through the person parameter and console.log the foll
 
 "President person was a great leader."
 */
-
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+for(var i = 0; i<presidents.length; i++){
+  console.log('The value at ' + i + ' is ' + presidents[i]) 
+}
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
